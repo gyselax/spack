9b6c63bae40d61c1113c0247fe5e1a51d9e43c17
Thomas Padioleau <thomas.padioleau@cea.fr>
2025-08-21 15:29:38 +0200

    Fix Kokkos recipe

--- packages/kokkos/package.py
+++ packages/kokkos/package.py
@@ -436,12 +436,11 @@ class Kokkos(CMakePackage, CudaPackage, ROCmPackage):
             if option:
                 spack_options.append(option)
 
-    @property
-    def kokkos_cxx(self) -> str:
-        if self.spec.satisfies("+wrapper"):
-            return self.spec["kokkos-nvcc-wrapper"].kokkos_cxx
-        # Assumes build-time globals have been set already
-        return spack_cxx
+    def setup_dependent_package(self, module, dependent_spec):
+        if "+wrapper" in self.spec:
+            self.spec.kokkos_cxx = self.spec["kokkos-nvcc-wrapper"].kokkos_cxx
+        else:
+            self.spec.kokkos_cxx = spack_cxx
 
     def cmake_args(self):
         spec = self.spec
