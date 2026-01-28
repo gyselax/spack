1d77827bda08c2eccd23e5f064b85381e375b14b
Thomas Padioleau <thomas.padioleau@cea.fr>
2026-01-28 15:15:55 +0100

    Update koliop

--- spack_repo/patches_v2025_11/packages/koliop/package.py
+++ spack_repo/patches_v2025_11/packages/koliop/package.py
@@ -3,35 +3,46 @@
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
 from spack_repo.builtin.build_systems.cmake import CMakePackage
+
 from spack.package import *
 
 
 class Koliop(CMakePackage):
-    """FIXME: Put a proper description of your package here."""
+    """A KOkkos based colLIsion OPerator (KoLiOp) that computes the evolution of the distribution function due to collisions."""
 
     homepage = "https://gitlab.com/cines/code.gysela/libkoliop"
-    git = "https://gitlab.com/cines/code.gysela/libkoliop.git"
-    url = "https://gitlab.com/cines/code.gysela/libkoliop/-/archive/0.0.21/libkoliop-0.0.21.tar.gz"
+    url = "https://gitlab.com/cines/code.gysela/libkoliop/-/archive/v0.1.0/libkoliop-v0.1.0.tar.gz"
 
     maintainers("etiennemlb", "tpadioleau")
 
     license("MIT", checked_by="tpadioleau")
 
-    version("0.0.21")
+    version("master", branch="master", no_cache=True)
+    version("0.1.0", sha256="511df587fba11c16e728d1ccdc68c9004dcb0cc87c548e955599d69bf68e6642")
 
-    depends_on("kokkos")
-    depends_on("kokkos-kernels")
+    depends_on("cxx", type="build")
+    depends_on("cmake@3.25:4", type="build")
+
+    depends_on("kokkos@4.1:4")
+    depends_on("kokkos-kernels@4.1:4")
 
     def cmake_args(self):
         args = [
-            self.define("koliop_ASSERT_ENABLED", False),
+            self.define("koliop_ASSERT_ENABLED", True),
             self.define("koliop_ASSUME_INPUT_BUFFERS_ARE_DEVICE_COMPATIBLE", True),
             self.define("koliop_BUILD_FORTRAN_INTERFACE", False),
             self.define("koliop_BUILD_TESTING", False),
+            self.define("koliop_CONSTANT_MEMORY_USAGE", False),
             self.define("koliop_ENABLE_Kokkos", "SYSTEM"),
             self.define("koliop_ENABLE_KokkosKernels", "SYSTEM"),
             self.define("koliop_ENABLE_LTO", False),
-            self.define("koliop_FENCE_ON_OPERATOR_EXIT", True),
-            self.define("koliop_RUN_DEMONSTRATION_AS_TEST", False),
+            self.define("koliop_FENCE_ON_OPERATOR_EXIT", False),
+            self.define("koliop_UTILITY_FLATTEN_EXPECT_POW2_EXTENTS", False),
         ]
+
+        if self.spec.satisfies("^kokkos+rocm"):
+            args.append(self.define("CMAKE_CXX_COMPILER", self.spec["hip"].hipcc))
+        else:
+            args.append(self.define("CMAKE_CXX_COMPILER", self["kokkos"].kokkos_cxx))
+
         return args
