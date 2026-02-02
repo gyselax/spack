a853bf07d20f4455f73b6318be17f1e812f0d893
Thomas Padioleau <thomas.padioleau@cea.fr>
2026-02-02 13:17:49 +0100

    Add koliop 0.1.1 and 0.1.2

--- spack_repo/patches_v2025_11/packages/koliop/package.py
+++ spack_repo/patches_v2025_11/packages/koliop/package.py
@@ -8,9 +8,11 @@ from spack.package import *
 
 
 class Koliop(CMakePackage):
-    """A KOkkos based colLIsion OPerator (KoLiOp) that computes the evolution of the distribution function due to collisions."""
+    """A KOkkos based colLIsion OPerator (KoLiOp) that computes the evolution of the distribution
+    function due to collisions."""
 
     homepage = "https://gitlab.com/cines/code.gysela/libkoliop"
+    git = "https://gitlab.com/cines/code.gysela/libkoliop.git"
     url = "https://gitlab.com/cines/code.gysela/libkoliop/-/archive/v0.1.0/libkoliop-v0.1.0.tar.gz"
 
     maintainers("etiennemlb", "tpadioleau")
@@ -18,6 +20,8 @@ class Koliop(CMakePackage):
     license("MIT", checked_by="tpadioleau")
 
     version("master", branch="master", no_cache=True)
+    version("0.1.2", sha256="7581e8313b7ebc09b291ac486d7f6d03b55d7e0ce2e8ebf62863177bbb080fd1")
+    version("0.1.1", sha256="abf5a7187067a452a6a5e45dcd6502ef077f77a591839adf8d21ed4e8b9987f7")
     version("0.1.0", sha256="511df587fba11c16e728d1ccdc68c9004dcb0cc87c548e955599d69bf68e6642")
 
     depends_on("cxx", type="build")
