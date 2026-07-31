e4ef019179d344e843f793d99938d7ed7121662c
Thomas Padioleau <thomas.padioleau@cea.fr>
2026-07-31 19:35:15 +0200

    Add mini app io

---
+++ spack_repo/patches_common/packages/gysela_mini_app_io/package.py
@@ -0,0 +1,41 @@
+# Copyright Spack Project Developers. See COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack_repo.builtin.build_systems.cmake import CMakePackage
+
+from spack.package import *
+
+
+class GyselaMiniAppIo(CMakePackage):
+    """Mini-application for IO testing and performance benchmarking"""
+
+    homepage = "https://gyselax.github.io/gyselalibxx/"
+    git = "https://github.com/tpadioleau/gysela-mini-app_io.git"
+
+    maintainers("tpadioleau")
+
+    license("MIT", checked_by="tpadioleau")
+
+    version("develop", branch="wip-rework-cmake", no_cache=True)
+
+    depends_on("cxx", type="build")
+    depends_on("cmake@3.25:4", type="build")
+
+    depends_on("ddc +pdi")
+    depends_on("gyselalibxx")
+    depends_on("mpi")
+    depends_on("paraconf")
+    depends_on("pdi")
+
+    def cmake_args(self):
+        args = [
+            self.define("GYSELA_MINI_APP_BUILD_SIMULATIONS", True),
+        ]
+
+        if self.spec.satisfies("^kokkos+rocm"):
+            args.append(self.define("CMAKE_CXX_COMPILER", self.spec["hip"].hipcc))
+        else:
+            args.append(self.define("CMAKE_CXX_COMPILER", self["kokkos"].kokkos_cxx))
+
+        return args
