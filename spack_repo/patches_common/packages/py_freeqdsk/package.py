e73dd827709d522d1b77dbf3932dca822db0b4bd
Thomas Padioleau <thomas.padioleau@cea.fr>
2026-09-10 19:57:12 +0200

    Add FreeQDSK

---
+++ spack_repo/patches_common/packages/py_freeqdsk/package.py
@@ -0,0 +1,28 @@
+# Copyright Spack Project Developers. See COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+
+from spack_repo.builtin.build_systems.python import PythonPackage
+
+from spack.package import *
+
+
+class PyFreeqdsk(PythonPackage):
+    """"""
+
+    homepage = "https://freeqdsk.readthedocs.io/"
+    git = "https://github.com/freegs-plasma/FreeQDSK.git"
+    # pypi = ""
+
+    license("MIT", checked_by="tpadioleau")
+
+    version("main", branch="main")
+    version("0.5.2")
+
+    depends_on("py-setuptools@65:", type="build")
+    depends_on("py-setuptools-scm@8: +toml", type="build")
+
+    depends_on("python@3.9:", type=("build", "run"))
+    depends_on("py-numpy@1.22:", type=("build", "run"))
+    depends_on("py-fortranformat@2", type=("build", "run"))
